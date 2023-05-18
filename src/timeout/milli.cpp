f6958c0429ea2b516962660fcbeec42d15992113
LDericher <40151420+ldericher@users.noreply.github.com>
2023-05-18 23:33:27 +0200

    timeout "milli"

---
+++ src/timeout/milli.cpp
@@ -0,0 +1,57 @@
+#include "milli.hpp"
+
+#include <stm32f1xx.h>
+
+#define TICK_TIME 1
+
+namespace timeout {
+
+bool Milli::hw_initialized_ = false;
+Timeout* Milli::first_ = nullptr;
+
+void Milli::TickTimers_ms() {
+  CLEAR_BIT(TIM3->SR, TIM_SR_UIF);
+
+  if (first_ != nullptr) {
+    ((Milli*)first_)->TickAll();
+  }
+}
+
+void Milli::InitHardware() {
+  if (!hw_initialized_) {
+    // ### USE TIM3 ###
+
+    // enable TIM3 clock
+    SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM3EN);
+
+    // PSC, ARR: 1 tick per TICK_TIME ms
+    TIM3->PSC = TICK_TIME * 1000 - 1;
+    TIM3->ARR = SystemCoreClock / 1000000UL;
+
+    // enable counter
+    SET_BIT(TIM3->CR1, TIM_CR1_CEN);
+  }
+}
+
+Milli::Milli() : Timeout(TICK_TIME, first_) {
+  InitHardware();
+}
+
+void Milli::StartHardware() {
+  // reset timer
+  TIM3->CNT = 0;
+
+  // enable interrupt
+  CLEAR_BIT(TIM3->SR, TIM_SR_UIF);
+  SET_BIT(TIM3->DIER, TIM_DIER_UIE);
+  NVIC_EnableIRQ(TIM3_IRQn);
+}
+
+void Milli::StopHardware() {
+  // disable interrupt
+  NVIC_DisableIRQ(TIM3_IRQn);
+  CLEAR_BIT(TIM3->DIER, TIM_DIER_UIE);
+  CLEAR_BIT(TIM3->SR, TIM_SR_UIF);
+}
+
+}  // namespace timeout
---
+++ src/timeout/milli.hpp
@@ -0,0 +1,55 @@
+#ifndef LIB_STM32_TIMEOUT_SRC_TIMEOUT_MILLI_H_
+#define LIB_STM32_TIMEOUT_SRC_TIMEOUT_MILLI_H_
+
+// local includes
+#include "timeout.hpp"
+
+// parent includes
+// #include "../timer/milli.h"
+
+// namespace timer {
+// class Milli;
+// }
+
+namespace timeout {
+
+/**
+ * @brief triggers after a set amount of milliseconds, uses TIM3
+ */
+class Milli : public Timeout {
+ private:
+  /// true if and only if TIM3 has been initialized
+  static bool hw_initialized_;
+
+  /// the first object of this class
+  static Timeout* first_;
+
+  /// the TIM3 interrupt
+  static void TickTimers_ms() asm("TIM3_IRQHandler");
+
+  /// initialize TIM3
+  static void InitHardware();
+
+  // // allow timer::Milli to call InitHardware
+  // friend class timer::Milli;
+
+ public:
+  /**
+   * @brief Construct a new Milli object
+   */
+  Milli();
+
+  /**
+   * @brief Start TIM3 interrupt
+   */
+  static void StartHardware();
+
+  /**
+   * @brief Stop TIM3 interrupt
+   */
+  static void StopHardware();
+};
+
+}  // namespace timeout
+
+#endif /* LIB_STM32_TIMEOUT_SRC_TIMEOUT_MILLI_H_ */
